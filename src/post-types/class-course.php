ab9248d6949095941036acd39b0d467c13524c31
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2020-07-21 13:04:04 -0400

    using Plugin Header Text Domain instead of hard-coded value; toggable course archive

--- src/post-types/class-course.php
+++ src/post-types/class-course.php
@@ -18,12 +18,12 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  * @package Badge_Factor_2
+ *
+ * @phpcs:disable WordPress.WP.I18n.NonSingularStringLiteralDomain
  */
 
 namespace BadgeFactor2\Post_Types;
 
-use BadgeFactor2\Models\BadgeClass;
-use BadgeFactor2\Post_Types\Approver;
 
 /**
  * Course post type.
@@ -39,7 +39,8 @@ class Course {
 	public static function init_hooks() {
 		add_action( 'init', array( Course::class, 'init' ), 99 );
 		add_filter( 'post_updated_messages', array( Course::class, 'updated_messages' ), 10 );
-		add_action( 'cmb2_admin_init', array( Course::class, 'custom_meta_boxes' ), 10 );
+		add_action( 'cmb2_admin_init', array( Course::class, 'register_settings_metaboxes' ), 10 );
+		add_action( 'cmb2_admin_init', array( Course::class, 'register_cpt_metaboxes' ), 10 );
 	}
 
 
@@ -48,42 +49,45 @@ class Course {
 	 */
 	public static function init() {
 
+		$plugin_data          = get_plugin_data( __FILE__ );
+		$badgefactor2_options = get_option( 'bf2_courses_settings' );
+
 		register_post_type(
 			'course',
 			array(
 				'labels'            => array(
-					'name'                  => __( 'Courses', 'bf2-courses' ),
-					'singular_name'         => __( 'Course', 'bf2-courses' ),
-					'all_items'             => __( 'All Courses', 'bf2-courses' ),
-					'archives'              => __( 'Course Archives', 'bf2-courses' ),
-					'attributes'            => __( 'Course Attributes', 'bf2-courses' ),
-					'insert_into_item'      => __( 'Insert into Course', 'bf2-courses' ),
-					'uploaded_to_this_item' => __( 'Uploaded to this Course', 'bf2-courses' ),
-					'featured_image'        => _x( 'Featured Image', 'course', 'bf2-courses' ),
-					'set_featured_image'    => _x( 'Set featured image', 'course', 'bf2-courses' ),
-					'remove_featured_image' => _x( 'Remove featured image', 'course', 'bf2-courses' ),
-					'use_featured_image'    => _x( 'Use as featured image', 'course', 'bf2-courses' ),
-					'filter_items_list'     => __( 'Filter Courses list', 'bf2-courses' ),
-					'items_list_navigation' => __( 'Courses list navigation', 'bf2-courses' ),
-					'items_list'            => __( 'Courses list', 'bf2-courses' ),
-					'new_item'              => __( 'New Course', 'bf2-courses' ),
-					'add_new'               => __( 'Add New', 'bf2-courses' ),
-					'add_new_item'          => __( 'Add New Course', 'bf2-courses' ),
-					'edit_item'             => __( 'Edit Course', 'bf2-courses' ),
-					'view_item'             => __( 'View Course', 'bf2-courses' ),
-					'view_items'            => __( 'View Courses', 'badgefactor2' ),
-					'search_items'          => __( 'Search Courses', 'badgefactor2' ),
-					'not_found'             => __( 'No Courses found', 'badgefactor2' ),
-					'not_found_in_trash'    => __( 'No Courses found in trash', 'badgefactor2' ),
-					'parent_item_colon'     => __( 'Parent Course:', 'badgefactor2' ),
-					'menu_name'             => __( 'Courses', 'badgefactor2' ),
+					'name'                  => __( 'Courses', $plugin_data['TextDomain'] ),
+					'singular_name'         => __( 'Course', $plugin_data['TextDomain'] ),
+					'all_items'             => __( 'All Courses', $plugin_data['TextDomain'] ),
+					'archives'              => __( 'Course Archives', $plugin_data['TextDomain'] ),
+					'attributes'            => __( 'Course Attributes', $plugin_data['TextDomain'] ),
+					'insert_into_item'      => __( 'Insert into Course', $plugin_data['TextDomain'] ),
+					'uploaded_to_this_item' => __( 'Uploaded to this Course', $plugin_data['TextDomain'] ),
+					'featured_image'        => _x( 'Featured Image', 'course', $plugin_data['TextDomain'] ),
+					'set_featured_image'    => _x( 'Set featured image', 'course', $plugin_data['TextDomain'] ),
+					'remove_featured_image' => _x( 'Remove featured image', 'course', $plugin_data['TextDomain'] ),
+					'use_featured_image'    => _x( 'Use as featured image', 'course', $plugin_data['TextDomain'] ),
+					'filter_items_list'     => __( 'Filter Courses list', $plugin_data['TextDomain'] ),
+					'items_list_navigation' => __( 'Courses list navigation', $plugin_data['TextDomain'] ),
+					'items_list'            => __( 'Courses list', $plugin_data['TextDomain'] ),
+					'new_item'              => __( 'New Course', $plugin_data['TextDomain'] ),
+					'add_new'               => __( 'Add New', $plugin_data['TextDomain'] ),
+					'add_new_item'          => __( 'Add New Course', $plugin_data['TextDomain'] ),
+					'edit_item'             => __( 'Edit Course', $plugin_data['TextDomain'] ),
+					'view_item'             => __( 'View Course', $plugin_data['TextDomain'] ),
+					'view_items'            => __( 'View Courses', $plugin_data['TextDomain'] ),
+					'search_items'          => __( 'Search Courses', $plugin_data['TextDomain'] ),
+					'not_found'             => __( 'No Courses found', $plugin_data['TextDomain'] ),
+					'not_found_in_trash'    => __( 'No Courses found in trash', $plugin_data['TextDomain'] ),
+					'parent_item_colon'     => __( 'Parent Course:', $plugin_data['TextDomain'] ),
+					'menu_name'             => __( 'Courses', $plugin_data['TextDomain'] ),
 				),
 				'public'            => true,
 				'hierarchical'      => false,
 				'show_ui'           => true,
 				'show_in_nav_menus' => true,
 				'supports'          => array( 'title', 'editor' ),
-				'has_archive'       => true,
+				'has_archive'       => ( isset( $badgefactor2_options['bf2_courses_use_archive'] ) && 'on' === $badgefactor2_options['bf2_courses_use_archive'] ),
 				'rewrite'           => true,
 				'query_var'         => true,
 				'menu_position'     => null,
@@ -136,11 +140,44 @@ class Course {
 
 
 	/**
-	 * Custom meta boxes.
-	 *
-	 * @return void
+	 * Registers Add-On settings page.
 	 */
-	public static function custom_meta_boxes() {
+	public static function register_settings_metaboxes() {
+		$plugin_data = get_plugin_data( __FILE__ );
+
+		$args = array(
+			'id'           => 'bf2_courses_settings_page',
+			'menu_title'   => __( 'Courses', $plugin_data['TextDomain'] ),
+			'object_types' => array( 'options-page' ),
+			'option_key'   => 'bf2_courses_settings',
+			'parent_slug'  => 'badgefactor2',
+			'tab_group'    => 'badgefactor2',
+			'tab_title'    => __( 'Courses', $plugin_data['TextDomain'] ),
+		);
+
+		// 'tab_group' property is supported in > 2.4.0.
+		if ( version_compare( CMB2_VERSION, '2.4.0' ) ) {
+			$args['display_cb'] = 'badgefactor2_options_display_with_tabs';
+		}
+
+		$plugins = new_cmb2_box( $args );
+
+		$plugins->add_field(
+			array(
+				'name' => __( 'Use Courses Archive?', $plugin_data['TextDomain'] ),
+				'desc' => __( 'If you enable this, a course archive will be automatically created. When you modify this, you need to flush your rewrite rules.', $plugin_data['TextDomain'] ),
+				'id'   => 'bf2_courses_use_archive',
+				'type' => 'checkbox',
+			)
+		);
+	}
+
+	/**
+	 * Registers Add-On settings page.
+	 */
+	public static function register_cpt_metaboxes() {
+		$plugin_data = get_plugin_data( __FILE__ );
+
 	}
 
 
@@ -166,7 +203,7 @@ class Course {
 				}
 				$courses[ $cat->term_id ]['posts'][] = $post;
 				break;
-			}	
+			}
 		}
 		return $courses;
 	}
