64237e0c5a534eb0bf0f4cab9f260d4e098f3a23
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2021-02-12 07:57:00 -0500

    templates cleanup

--- src/controllers/class-course-controller.php
+++ src/controllers/class-course-controller.php
@@ -24,7 +24,12 @@
 
 namespace BadgeFactor2\Controllers;
 
+use BadgeFactor2\BadgrProvider;
+use BadgeFactor2\BadgrUser;
+use BadgeFactor2\Models\BadgeClass;
+use BadgeFactor2\Models\Issuer;
 use BadgeFactor2\Page_Controller;
+
 
 /**
  * Course Controller Class.
@@ -57,6 +62,65 @@ class Course_Controller extends Page_Controller {
 	 * @return void|string
 	 */
 	public static function single( $default_template = null ) {
+
+		global $post;
+
+		$current_user = wp_get_current_user();
+
+		// The WooCommerce add-on is installed.
+		if ( class_exists( 'BadgeFactor2\BF2_WooCommerce' ) ) {
+			// A product is linked to the course.
+			$product_id = get_post_meta( $post->ID, 'course_product', true );
+			if ( $product_id ) {
+				// The client has not purchased this product, redirect to the product page.
+				if ( ! wc_customer_bought_product( $current_user->user_email, $current_user->ID, $product_id ) ) {
+					echo sprintf( '<script>window.location.replace("%s")</script>', get_permalink( $product_id ) );
+					die;
+				}
+			}
+		}
+		
+		$options     = get_option( 'badgefactor2' );
+
+		$fields = array();
+
+		$fields['form_slug']                = ! empty( $options['bf2_form_slug'] ) ? $options['bf2_form_slug'] : 'form';
+		$fields['autoevaluation_form_slug'] = ! empty( $options['bf2_autoevaluation_form_slug'] ) ? $options['bf2_autoevaluation_form_slug'] : 'autoevaluation';
+		$fields['current_user']             = $current_user;
+		$fields['badgr_user']               = new BadgrUser( $fields['current_user'] );
+		$fields['course']                   = $post;
+		$fields['badge_page']               = get_post( get_post_meta( $fields['course']->ID, 'course_badge_page', true ) );
+		$fields['badge_entity_id']          = get_post_meta( $fields['badge_page']->ID, 'badgepage_badge', true );
+		$fields['badge_page_id']            = get_post_meta( $post->ID, 'course_badge_page', true );
+		$fields['badge_entity_id']          = get_post_meta( $fields['badge_page_id'], 'badge', true );
+		$fields['badge']                    = BadgeClass::get( $fields['badge_entity_id'] );
+		$fields['issuer']                   = $fields['badge'] ? Issuer::get( $fields['badge']->issuer ) : '';
+		$fields['autoevaluation_form']      = is_plugin_active( 'bf2-gravityforms/bf2-gravityforms.php' ) ? get_post_meta( $fields['badge_page']->ID, 'autoevaluation_form_id', true ) : '';
+		$fields['form_type']                = get_post_meta( $fields['badge_page']->ID, 'badge_request_form_type', true );
+		$fields['backpack']                 = BadgrProvider::get_all_assertions_from_user_backpack( $fields['badgr_user'] );
+		$fields['assertion']                = null;
+		$fields['issued']                   = false;
+		$fields['revoked']                  = false;
+		$fields['issued_on']                = null;
+		$fields['course_categories']        = wp_get_post_terms( $fields['course']->ID, 'course-category', array( 'fields' => 'all' ) );
+		$fields['course_titles']            = wp_get_post_terms( $fields['course']->ID, 'course-title', array( 'fields' => 'all' ) );
+		$fields['course_level']             = wp_get_post_terms( $fields['course']->ID, 'course-level', array( 'fields' => 'all' ) );
+
+
+		foreach ( $fields['backpack'] as $item ) {
+			if ( $item->badgeclass === $fields['badge_entity_id'] ) {
+				$fields['assertion'] = $item;
+				$fields['issued']    = true;
+				$fields['revoked']   = $fields['assertion']->revoked;
+				$fields['issued_on'] = gmdate( 'Y-m-d', strtotime( $fields['assertion']->issuedOn ) );
+				break;
+			}
+		}
+
+		global $bf2_template;
+		$bf2_template         = new \stdClass();
+		$bf2_template->fields = $fields;
+
 		return parent::single( $default_template );
 	}
 
